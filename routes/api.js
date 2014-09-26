f56ae7723f0286657dde6aab0bb31116d9c59d87
Raibima Imam Putra <rayp1100@gmail.com>
2014-09-26 23:31:35 +0700

    fix indentation (convert tabs to spaces)

--- routes/api.js
+++ routes/api.js
@@ -10,27 +10,27 @@ router.route('/user')
     // GET /api/user -> all user data
     .get(function(req, res) {
         db.User.findAll({
-			attributes: ['username', 'email']
-		})
+            attributes: ['username', 'email']
+        })
             .success(function(users) {
                 res.json(users);
             });
     })
-	
-	.post(function(req,res) {
-		// Create new user entry
-		db.User.create({
-			username: req.body.username,
-			password: req.body.password,
-			email: req.body.email
-		}).complete(function(err) {
-			if (err) {
-				res.json({ message: 'error' });
-			} else {
-				res.json({ message: 'success'});
-			}
-		});
-		
-	});
+    
+    // POST /api/user -> create new user
+    .post(function(req,res) {
+        db.User.create({
+            username: req.body.username,
+            password: req.body.password,
+            email: req.body.email
+        }).complete(function(err) {
+            if (err) {
+                res.json({ message: 'error' });
+            } else {
+                res.json({ message: 'success'});
+            }
+        });
+        
+    });
 
 module.exports = router;
