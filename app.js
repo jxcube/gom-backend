b6cfdc0b6b7b895e3037def83e20143c21355f0f
coderay1100 <rayp1100@gmail.com>
2014-09-30 07:59:07 +0700

    remove 'api' from route

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // set up the route
-app.use('/api', api);
+app.use('/', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
