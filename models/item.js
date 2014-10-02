30474d3dc10c48b4c921ccb7fe309765d2d01018
yonasrnd <renrex@hotmail.com>
2014-10-02 08:34:31 +0700

    fix error

--- models/item.js
+++ models/item.js
@@ -2,7 +2,8 @@ var model =function(sequelize,DataTypes){
 	var Item = sequelize.define('Item',{
 		name: DataTypes.STRING,
 		price: DataTypes.DECIMAL(15,2),
-		description: DataTypes.TEXT
+		description: DataTypes.TEXT,
+		tag : DataTypes.STRING
 	}, {});
 	return Item;
 }
