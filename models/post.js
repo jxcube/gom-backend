a8b82ab9617941c8e06a67e76d6e1251ad0df01f
coderay1100 <rayp1100@gmail.com>
2014-11-05 10:46:56 +0700

    bug fix

--- models/post.js
+++ models/post.js
@@ -11,6 +11,7 @@ module.exports = function(sequelize, Datatypes){
         }
     });
 
+    return Post;
 }
 
 
--- models/thread.js
+++ models/thread.js
@@ -9,6 +9,8 @@ module.exports = function(sequelize, Datatypes){
             }
         }
     });
+
+    return Thread;
 }
 
 
